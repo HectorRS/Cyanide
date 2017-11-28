25944d4efb604404cbe383760cae8f09799bad20
Scarlett Azure <thienanh1712@gmail.com>
2017-11-29 00:57:15 +0800

    Add project files.

---
+++ Cyanide/Modules/User Interaction/Cyanide.Soar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Cyanide.Modules.User_Interaction
+{
+    public class CyanCheck : ModuleBase<SocketCommandContext>
+    {
+        [Command("soar")]
+        [Summary("Soars like a beast.")]
+        public async Task SoarAsync()
+        {
+            await ReplyAsync("...Meow.");
+        }
+
+    }
+}
---
+++ Cyanide/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+
+namespace Cyanide
+{ 
+    public class CyanProgram
+    {
+        private DiscordSocketClient CyanClient;
+        private CommandService CyanCommands;
+        private IServiceProvider CyanServices;
+
+        public static void Main(string[] args)
+            => new CyanProgram().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            CyanClient = new DiscordSocketClient();
+            CyanCommands = new CommandService();
+            CyanServices = new ServiceCollection()
+                .AddSingleton(CyanClient)
+                .AddSingleton(CyanCommands)
+                .BuildServiceProvider();
+
+            CyanClient.Log += CyanLog;
+
+            await InstallCommandsAsync();
+
+            await CyanClient.LoginAsync(TokenType.Bot, CyanConfig.Load().Token);
+            await CyanClient.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            CyanClient.MessageReceived += HandleCommandAsync;
+            await CyanCommands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+
+            if (message == null)
+                return;
+
+            int argPos = 0;
+
+            if (!(message.HasStringPrefix("Cyan ", ref argPos) || message.HasMentionPrefix(CyanClient.CurrentUser, ref argPos)))
+                return;
+
+            var context = new SocketCommandContext(CyanClient, message);
+
+            var result = await CyanCommands.ExecuteAsync(context, argPos, CyanServices);
+
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+        private Task CyanLog(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+
+    public class CyanConfig
+    {
+        public string Token { get; set; }
+
+        public CyanConfig()
+        {
+            Token = "";
+        }
+
+        public static CyanConfig Load(string dir = "Configuration.json")
+        {
+            string file = Path.Combine(AppContext.BaseDirectory, dir);
+            return JsonConvert.DeserializeObject<CyanConfig>(File.ReadAllText(file));
+        }
+    }
+}
